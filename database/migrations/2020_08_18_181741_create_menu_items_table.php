b4238c2aab48d61ce016e3139fd60c4d62c25691
phuchp1997 <phuchp1997@gmail.com>
2021-03-12 20:10:33 -0500

    update

--- database/migrations/2020_08_18_181741_create_menu_items_table.php
+++ database/migrations/2020_08_18_181741_create_menu_items_table.php
@@ -18,7 +18,7 @@ class CreateMenuItemsTable extends Migration
             $table->string('name');
             $table->text('description');
             $table->unsignedBigInteger('item_type_id')->references('id')->on('item_types');
-            $table->string('item_image')->default('../uploads/FoodImage/pizza.jpg');
+            $table->string('item_image')->default('../uploads/FoodImage/default.jpg');
             $table->timestamps();
         });
     }
